de5f5e8414967ff24d7ecfd9fd4d40d04a197bcb
HuachaDev <luismickey2198@gmail.com>
2024-06-23 14:21:18 -0500

    script para ocultar inspector de codigo web

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nombre del Proyecto
+hideFrontendSC
+
+## Uso
+Script que oculta el inspector de codigo  mediante teclado y click derecho para usuarios
+no tecnicos.
---
+++ app.js
@@ -0,0 +1,15 @@
+
+(() =>{
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener('keydown', function(e) {
+        if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74))) {
+            e.preventDefault();
+        }
+    });
+})();
+
+
+
